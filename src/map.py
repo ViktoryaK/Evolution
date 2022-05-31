8e7b8a8f4e2efd64794c4ed4bd5845360c46fe20
mr <92575094+yakorch@users.noreply.github.com>
2022-05-31 13:41:05 +0300

    Add files via upload

--- src/map.py
+++ src/map.py
@@ -7,7 +7,8 @@ Processes each creature's move, checks if a move is valid.
               For commercial use only.
 -----------------------------------------------------------
 """
-import random
+from copy import deepcopy
+from random import sample
 from phage import *
 from brain import *
 
@@ -50,28 +51,44 @@ class Map:
         """
         free_squares = [(height, length) for height in range(self.size) for length in range(self.size) if
                         self.map[height][length] is None]
-        return random.sample(free_squares, number)
+        return sample(free_squares, number)
 
     def __repr__(self):
         return "\n".join([str([item for item in self.map[i]]) for i in range(self.size)])
 
-    def give_to_vika(self) -> list[list]:
+    @staticmethod
+    def give_to_vika(one_board: list[list]) -> list[list]:
         """
         Simplifies a map for showing it on a video
         """
-        return [[repr(item) for item in self.map[i]] for i in range(self.size)]
+        return [[repr(item) for item in one_board[i]] for i in range(len(one_board))]
 
     def cycle(self, generations: int) -> list[list[list]]:
         """
         Runs a simulation 'generations' times
         Puts each state into a list to be processed later
         """
+        all_states = []
         for i in range(generations):
-            pass
-        pass
+            # iterating through map, asking creatures their desires
+            phage_wantings = dict()
+            for hey, row in enumerate(self.map):
+                for leng, elem in enumerate(row):
+                    if elem is not None:
+                        dx, dy = None, None
+                        # TODO: NAZAR WTF шо то dx i dy таке
+                        phage_wantings[(hey, leng)] = elem.get_next_move(dx, dy)
+
+            # performing what they want:
+            # ....
+
+            all_states.append(deepcopy(self.map))
+        return all_states
 
 
 if __name__ == "__main__":
     board = Map(100)
     board.generate_creatures(num_of_enemies=40, num_of_preys=100)
+    simulation = board.cycle(100)
+    give_vika = list(map(lambda state: Map.give_to_vika(state), simulation))
     # print(board)
